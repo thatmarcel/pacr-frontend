832f60b41e1f12e4b6b1675df9e9b082402dad8a
Marcel Braun <contact@thatmarcel.com>
2021-12-15 22:35:32 +0100

    Add about button

--- components/HeaderBar.jsx
+++ components/HeaderBar.jsx
@@ -1,7 +1,24 @@
+import {
+    IconButton,
+    Button,
+
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+
+    Link as ChakraLink
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { InfoOutlineIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import strings from "../misc/strings.json";
 
 const HeaderBar = ({ hideBarBottomShadow }) => {
+    const { isOpen: isAboutModalOpen, onOpen: onAboutModalOpen, onClose: onAboutModalClose } = useDisclosure();
+
     return (
         <div className={`w-full h-16 flex ${hideBarBottomShadow ? "" : "border-b-2 border-gray"}`}>
             <Link href="/">
@@ -9,6 +26,38 @@ const HeaderBar = ({ hideBarBottomShadow }) => {
                     {strings.appName}
                 </a>
             </Link>
+
+            <div className="grow" /> {/* Spacer */}
+
+            <IconButton marginY="auto" marginRight={4} icon={<InfoOutlineIcon />} onClick={onAboutModalOpen} />
+
+            <Modal isOpen={isAboutModalOpen} onClose={onAboutModalClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>
+                        {strings.aboutThisSite}
+                    </ModalHeader>
+                    <ModalBody>
+                        <ChakraLink isExternal={true} display="block" href="https://github.com/thatmarcel/pacr-frontend">
+                            {strings.goToFrontendSourceCode} <ExternalLinkIcon mx="2px" />
+                        </ChakraLink>
+
+                        <ChakraLink isExternal={true} marginTop={4} marginBottom={8} display="block" href="https://github.com/thatmarcel/pacr-backend">
+                            {strings.goToBackendSourceCode} <ExternalLinkIcon mx="2px" />
+                        </ChakraLink>
+
+                        <p>
+                            {strings.acknowledgementsText}
+                        </p>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button variant="ghost" onClick={onAboutModalClose}>
+                            {strings.close}
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </div>
     );
 }
