c2571a89d09a83345533542f45d2c9b1bfb0019a
Marcel Braun <contact@thatmarcel.com>
2021-12-15 22:16:53 +0100

    Add simple home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,30 @@
 import DefaultContainer from "../components/DefaultContainer";
 
+import {
+    Button
+} from "@chakra-ui/react";
+
+import strings from "../misc/strings.json";
+import Link from "next/link";
+
 const Index = () => {
     return (
         <DefaultContainer>
-            <p>Hello</p>
+            <div className="p-8">
+                <h1 className="font-extrabold text-2xl md:text-5xl mb-6 mt-16 text-center">
+                    {strings.indexPageTitle}
+                </h1>
+
+                <div className="flex">
+                    <Link href="/editor">
+                        <a className="mx-auto">
+                            <Button>
+                                {strings.goToEditorButtonTitle}
+                            </Button>
+                        </a>
+                    </Link>
+                </div>
+            </div>
         </DefaultContainer>
     );
 }
