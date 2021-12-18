62ac43e89cf8199f55992a38d40de627a3dbc16c
Marcel Braun <contact@thatmarcel.com>
2021-12-18 16:06:07 +0100

    Remove unused import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/global.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
 import PlausibleProvider from "next-plausible";
