e684cebcfcc50486a2089f0a079a321fe29fae82
Marcel Braun <contact@thatmarcel.com>
2021-12-18 16:05:56 +0100

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import "../styles/global.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ChakraProvider>
-            <DndProvider backend={HTML5Backend}>
-                <Component {...pageProps} />
-            </DndProvider>
-        </ChakraProvider>
+        <PlausibleProvider domain="pacr.app">
+            <ChakraProvider>
+                <DndProvider backend={HTML5Backend}>
+                    <Component {...pageProps} />
+                </DndProvider>
+            </ChakraProvider>
+        </PlausibleProvider>
     );
 }
 
