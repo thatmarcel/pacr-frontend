38005684aa340d567727ae6496ed3a7ef3425e41
Marcel Braun <contact@thatmarcel.com>
2022-01-17 20:24:10 +0100

    Improve ip / mask matching via library that can also check mask parts apart from 255 and 0

--- helpers/simulation/check_if_ip_matches_mask.js
+++ helpers/simulation/check_if_ip_matches_mask.js
@@ -1,29 +1,7 @@
+import { matches } from "ip-matching";
+
 const checkIfIpMatchesMask = (ipAddress, maskIpAddress, mask) => {
-    const ipParts = ipAddress.split(".").map(part => parseInt(part)).filter(part => part !== NaN);
-    const maskIpParts = maskIpAddress.split(".").map(part => parseInt(part)).filter(part => part !== NaN);
-    const maskParts = mask.split(".").map(part => parseInt(part)).filter(part => part !== NaN);
-
-    if (ipParts.length !== 4 || maskIpParts.length !== 4 || maskParts.length !== 4) {
-        return false;
-    }
-
-    for (let index of [0, 1, 2, 3]) {
-        if (maskParts[index] === 0) {
-            continue;
-        }
-
-        if (maskParts[index] < 0 || maskParts[index] > 255) {
-            return false;
-        }
-
-        if (maskParts[index] === 255 && ipParts[index] !== maskIpParts[index]) {
-            return false;
-        }
-
-        // TODO: Check masks with numbers different than 0 and 255
-    }
-
-    return true;
+    return matches(ipAddress, `${maskIpAddress}/${mask}`);
 }
 
 export default checkIfIpMatchesMask;
